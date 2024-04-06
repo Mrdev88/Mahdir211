ef13018d72af5ea6a73ff7dd327f5ac65cba08c2
Mahdi <RuizMahdi@gmx.com>
2024-04-06 10:19:51 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hello")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("I like to do coding")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Welcome")
+})
+basic.showString("Hello, I'm Mahdir211. I like to do coding.")
